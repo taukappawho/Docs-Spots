22c300d163955bc291771649a718c7a0c4201e6f
Neal <nealbhalodia@gmail.com>
2021-03-21 20:11:35 -0400

    Removed unnessary content from footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,20 +6,6 @@ import './Footer.css'
 function Footer() {
   return (
       <div className="footer-container">
-          <section className="footer-subscription">
-              <p className="footer-subscription-heading">
-                  What to subscribe? Send us your email below!
-              </p>
-              <p className="footer-subscription-text">
-                  You can unsubscribe at anytime
-              </p>
-              <div className="input-areas">
-                  <form>
-                      <input type="email" name="email" placeholder="Your Email" className="footer-input"/>
-                      <Button buttonStyle="btn--outline">Subscribe</Button>
-                  </form>
-              </div>
-          </section>
           {/* <div className="footer-links">
               <div className="footer-link-wrapper">
                   <div className="footer-link-items">
