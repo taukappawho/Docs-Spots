2aea5a8a8b674e069c18ddebd14b2a22650c8c01
Neal <nealbhalodia@gmail.com>
2021-03-21 20:06:12 -0400

    Added Route for registration/login page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from './components/pages/Home';
 import Profile from './components/pages/Profile';
 import Chat from './components/pages/Chat';
 import Nearby from './components/pages/Nearby';
+import Register_login from './components/pages/Register_login';
 import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App(){
@@ -17,6 +18,7 @@ function App(){
         <Route path='/profile' component={Profile} />
         <Route path='/chat' component={Chat} />
         <Route path='/nearby' component={Nearby} />
+        <Route path='/Register_login' component={Register_login} />
       </Switch>
       <Footer/>
     </Router>
