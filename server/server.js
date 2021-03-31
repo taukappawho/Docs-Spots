dce467d262266d7894498f75f61eea81e3f353a5
arunkulkarni95 <arunkulkarni95@hotmail.com>
2021-03-30 20:44:13 -0400

    add check for production build

--- server/server.js
+++ server/server.js
@@ -3,13 +3,13 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
-require('dotenv').config();
+if (process.env.NODE_ENV !== "production") require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 corsOptions = {
-  origin: "https://docs-spots.herokuapp.com/",
+  origin: "https://docs-spots.herokuapp.com",
   optionsSuccessStatus: 200
 };
 
