f0bcd61004f6b0c8e9c240cd81a86dca7d5b41c1
James Naurot <naurotj@gmail.com>
2021-04-13 13:16:33 -0400

    removed register login path (commented out)

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Home from './components/pages/Home';
 import Profile from './components/pages/Profile';
 import Chat from './components/pages/Chat';
 import Nearby from './components/pages/Nearby';
-import Register_login from './components/pages/Register_login';
 import Log from './components/Log';
 import Logout from './components/Logout';
 import verifyId from './components/verifyId';
@@ -22,7 +21,7 @@ export default function App(){
         <ProtectedRoute path='/profile' comp={Profile} />
         <ProtectedRoute path='/chat' comp={Chat} />
         <ProtectedRoute path='/nearby' comp={Nearby} />
-        <ProtectedRoute path='/Register_login' comp={Register_login} />
+        <Route path="*" component={Home} />
       </Switch>
       <Footer/>
     </Router>
