de77e7165bd144f32447a755d449ed6996cf62bf
John Richard <john.b.richard@outlook.com>
2021-04-16 14:49:20 -0400

    update nav to show/hide based on login status

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -20,44 +20,69 @@ function Navbar(){
         showButton();
     },[]);
     window.addEventListener("resize", showButton);
-  return (
-    <>
-      <nav className="navbar">
-            <div className="navbarGroup">
-                {/* Displaying the title of the webpage */}
-                <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
-                    Doc's Spots
-                </Link>
-                <div className="menu-icon" onClick={handleClick}>
-                    {/* Changing the icon from an x to bars to mimic a menu */}
-                    <i className={click ? "fas fa-times" : "fas fa-bars"}/>
-                </div>
-                <ul className={click ? "nav-menu active" : "nav-menu"}>
-                    {/* Below are the tabs a user can click on */}
-                    <li className="nav-item">
-                        <Link to="/nearby" className="nav-links" onClick={closeMobileMenu}>
-                            Nearby
+    if(sessionStorage.token){
+        return (
+            <>
+              <nav className="navbar">
+                    <div className="navbarGroup">
+                        {/* Displaying the title of the webpage */}
+                        <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+                            Doc's Spots
                         </Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to="/profile" className="nav-links" onClick={closeMobileMenu}>
-                            Profile
+                        <div className="menu-icon" onClick={handleClick}>
+                            {/* Changing the icon from an x to bars to mimic a menu */}
+                            <i className={click ? "fas fa-times" : "fas fa-bars"}/>
+                        </div>
+                        <ul className={click ? "nav-menu active" : "nav-menu"}>
+                            {/* Below are the tabs a user can click on */}
+                            <li className="nav-item">
+                                <Link to="/nearby" className="nav-links" onClick={closeMobileMenu}>
+                                    Nearby
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/chat" className="nav-links" onClick={closeMobileMenu}>
+                                    Chat
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/profile" className="nav-links" onClick={closeMobileMenu}>
+                                    Profile
+                                </Link>
+                            </li>
+                            {/* This is the button used to have a user sign in to the webpage */}
+                            <li className="nav-item">
+                                <Log/>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </>
+          )
+    }
+    else{
+        return (
+            <>
+            <nav className="navbar">
+                    <div className="navbarGroup">
+                        {/* Displaying the title of the webpage */}
+                        <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+                            Doc's Spots
                         </Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to="/chat" className="nav-links" onClick={closeMobileMenu}>
-                            Chat
-                        </Link>
-                    </li>
-                    
-                    {/* This is the button used to have a user sign in to the webpage */}
-                    <li className="nav-item">
-                        <Log/>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    </>
-  )
+                        <div className="menu-icon" onClick={handleClick}>
+                            {/* Changing the icon from an x to bars to mimic a menu */}
+                            <i className={click ? "fas fa-times" : "fas fa-bars"}/>
+                        </div>
+                        <ul className={click ? "nav-menu active" : "nav-menu"}>
+                            {/* This is the button used to have a user sign in to the webpage */}
+                            <li className="nav-item">
+                                <Log/>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </>
+        )
+    }
 };
 export default Navbar;
--- src/components/TopSection.js
+++ src/components/TopSection.js
@@ -3,20 +3,21 @@ import './TopSection.css';
 import '../App.css';
 
 function TopSection() {
-    return (
-        //This js file is used to display some of the contents of the homepage.
-        //Such as a welcome message and a get started button 
-        <div className="top">
-            <h1>Welcome to Doc's Spots!</h1>
-            <p>Let's get started</p>
-            <div className="button">
-                {/* <Button className="theButton" 
-                buttonStyle="btn--outline" 
-                buttonSize="btn--large">
-                    START HERE
-                </Button> */}
+    if(sessionStorage.token){
+        return (
+            <div className="top">
+                <h1>Welcome to Doc's Spots!</h1>
+                <p>You've logged in successfully using your Towson account!</p>
             </div>
-        </div>
-    )
+        )
+    }
+    else{
+        return (
+            <div className="top">
+                <h1>Welcome to Doc's Spots!</h1>
+                <p>Log in with your Towson account to get started</p>
+            </div>
+        )
+    }
 }
 export default TopSection
