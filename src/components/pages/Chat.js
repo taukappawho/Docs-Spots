6e431cee61b5abe196287229e5be1738cd9b42ea
Benjamin King <bking23@students.towson.edu>
2021-04-20 22:36:51 -0400

    Began work on the chat feature. Current a bug is known with using the socket.io

--- src/components/pages/Chat.js
+++ src/components/pages/Chat.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import queryString from 'query-string';
+import io from 'socket.io-client';
 import {Container} from 'reactstrap';
 import '../../App.css'
+//let socket;
+function Chat({location}){
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
+  const ENDPOINT = 'localhost:5000'
+  useEffect(() => {
+    const {name, room} = queryString.parse(location.search);
+    var socket = io(ENDPOINT);
+    setName(name);
+    setRoom(room);
+    console.log(socket);
+  },[ENDPOINT, location.search]);
 
-function Chat(props){
   return(
-    <Container>
-      <h1>Chat</h1>
-      <Container>
-        <p align="center">This website is under construction! Check in later for updates!</p>
-      </Container>
-    </Container>
+    <h1>Chat</h1>
+    // <Container>
+    //   <h1>Chat</h1>
+    //   <Container>
+    //     <p align="center">This website is under construction! Check in later for updates!</p>
+    //   </Container>
+    // </Container>
   );
 }
 export default Chat;
